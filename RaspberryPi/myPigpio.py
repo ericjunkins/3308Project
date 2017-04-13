c0151395df7c160611834a3ae774fd0fc65de68b
ericjunkins <eric.junkins>
2017-04-12 21:57:05 -0600

    deleting uncessary/unused files for the project. Renamed wifi_socket_test.py to rcCar.py, as well as added all necessary code for talking to the wifi socket and receiving signals from the android app

---
+++ RaspberryPi/myPigpio.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+
+import time
+import pigpio
+
+'''
+#This file contains the methods used to control movement of the RC car. The
+#methods turn and throttle will be called on numbers that are precentages of
+#their max in each direction, and will output a signal and will not kill that
+#signal until they are called again. This reduces the number of necessary
+#function calls as well as speeds up the program by not having to talk to
+#gpio pins more than necessary. 
+'''
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#      Uncomment the section below to run a test Function from this program
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+pi2 = pigpio.pi()
+pi2.set_mode(14,pigpio.OUTPUT)
+base_thr = []
+base_thr.append(pigpio.pulse(1<<14,0,1.5*1000))
+base_thr.append(pigpio.pulse(0,1<<14, 8.5*1000))
+pi2.wave_add_generic(base_thr)
+th_wid = pi2.wave_create()
+if th_wid >= 0:
+        pi2.wave_send_repeat(th_wid)
+
+
+pi1 = pigpio.pi()
+pi1.set_mode(23,pigpio.OUTPUT)
+base_st = []
+base_st.append(pigpio.pulse(1<<23,0,1.5*1000))
+base_st.append(pigpio.pulse(0,1<<23, 8.5*1000))
+pi1.wave_add_generic(base_st)
+st_wid = pi1.wave_create()
+if st_wid >= 0:
+        pi1.wave_send_repeat(st_wid)
+'''
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Need to have an initial wave id of zero
+wid =0
+
+#throttle is the function to move the Rc car forward/backward
+def throttle(x,pi):
+        
+        #x is the percent of max to throttle forward (+) or backward(-)
+        if(x >=-100 and x<=100):
+                
+                #have to stop the instance of pi to not crash the raspberry pi
+                pi.wave_tx_stop()
+                
+                #throttle uses gpio pin 14
+                GPIO=14
+                square1=[]
+                square2=[]
+                pi = pigpio.pi()
+                pi.set_mode(GPIO,pigpio.OUTPUT)
+                
+                #if you tell the car to go backwards it has to either have gotten
+                #a negative value previously, or a baseline signal of 1.5
+                #TODO build in check for previous output for that case
+                if (x <= 0):
+                        
+                        #build base signal of 1.5ms on, 8.5 off
+                        square1.append(pigpio.pulse(1<<GPIO,0,1.5*1000))
+                        square1.append(pigpio.pulse(0,1<<GPIO,8.5*1000))
+
+                        pi.wave_add_generic(square1)
+                        wid = pi.wave_create()
+                        
+                        #if the wave id was created sucessfully
+                        if wid >=0:
+                                pi.wave_send_repeat(wid)
+                                time.sleep(float(1)/float(20))
+                                pi.wave_tx_stop()
+                                pi.wave_delete(wid)
+                                
+                #pulseLen is the signal corresponding to turning. This will be
+                #between 1.0ms and 2.0ms depending on input x
+                                
+                pulseLen = float(1.5)+(float(0.5)*(float(x)/float(100)))
+                square2.append(pigpio.pulse(1<<GPIO,0,pulseLen*1000))
+                square2.append(pigpio.pulse(0,1<<GPIO,(10 - pulseLen)*1000))
+                pi.wave_add_generic(square2)
+                wid = pi.wave_create()
+
+                if wid >=0:
+                        pi.wave_send_repeat(wid)
+                        pi.wave_delete(wid)
+                        pi.stop()
+                        '''
+                        #important to note the signal is NOT stopped from this,
+                        #it will continue until the function is called again
+                        #and recieves pi.wave_tx_stop()
+                        '''
+                return pi
+
+
+
+#turn is the function that control the steering of the RC car, it is almost
+#identical to throttle except for output pins and not requiring a base signal       
+def turn(x,pi):
+        if (x >=-100 and x <=100):
+                pi.wave_tx_stop()
+                
+                #steering uses gpio pin 23
+                GPIO = 23
+                square=[]
+                pulseLen = float(1.5)+(float(0.5)*(float(x)/float(100)))
+                square.append(pigpio.pulse(1<<GPIO,0,pulseLen*1000))
+                square.append(pigpio.pulse(0,1<<GPIO,(10 - pulseLen)*1000))
+                pi.wave_add_generic(square)
+                wid = pi.wave_create()
+                try:
+                        if wid	>= 0:
+
+                                pi.wave_delete(wid)
+                                pi.stop
+                except KeyboardInterrupt:
+                        turn(0,pi)
+                return pi
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                             For Unittesting
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+        
+class TextProcError(Exception):
+        def _init_(self,msg):
+
+                super().__init__(msg)
+
+class RcFuncs:
+                
+        def __init__(self,num):
+                if type(num) is not int:
+                        raise TextProcError("Requires int")
+                self.num = num
+
+        #Controls the steering of the Rc car
+        def turn(self):
+                x = self.num
+                #Signals are percents of turning, negative meaning left
+                if (x >=-100 and x <=100):
+                        #Steering uses gpio pin 23
+                        GPIO =23
+                        square=[]
+                        pi = pigpio.pi()
+                        pi.set_mode(GPIO,pigpio.OUTPUT)
+                        pulseLen = float(1.5)+(float(0.5)*(float(x)/float(100)))
+                        square.append(pigpio.pulse(1<<GPIO,0,pulseLen*1000))
+                        square.append(pigpio.pulse(0,1<<GPIO,(10 - pulseLen)*1000))
+                        pi.wave_add_generic(square)
+
+                        wid = pi.wave_create()
+
+                        if wid	>= 0:
+                                pi.wave_send_repeat(wid)
+                                time.sleep(float(1)/float(10))
+                                pi.wave_tx_stop()
+                                pi.wave_delete(wid)
+
+                                pi.stop
+                                
+                        return pulseLen
+        
+        #Controls the movemnt of the Rc car
+        def throttle(self):
+                x = self.num
+                if(x >=-100 and x<=100):
+                        #Throttle uses gpio pin 14
+                        GPIO=14
+                        square1=[]
+                        square2=[]
+                        pi = pigpio.pi()
+                        pi.set_mode(GPIO,pigpio.OUTPUT)
+
+                        square1.append(pigpio.pulse(1<<GPIO,0,1.5*1000))
+                        square1.append(pigpio.pulse(0,1<<GPIO,8.5*1000))
+
+                        pi.wave_add_generic(square1)
+
+                        wid = pi.wave_create()
+
+                        if wid >=0:
+                                pi.wave_send_repeat(wid)
+                                time.sleep(float(1)/float(10))
+                                pi.wave_tx_stop()
+                                pi.wave_delete(wid)
+
+                        pulseLen = float(1.5)+(float(0.5)*(float(x)/float(100)))
+                        square2.append(pigpio.pulse(1<<GPIO,0,pulseLen*1000))
+                        square2.append(pigpio.pulse(0,1<<GPIO,(10 - pulseLen)*1000))
+
+                        pi.wave_add_generic(square2)
+
+                        wid = pi.wave_create()
+
+                        if wid >=0:
+                                pi.wave_send_repeat(wid)
+                                try:
+                                        time.sleep(float(0.5))
+                                        pi.wave_tx_stop()
+                                        pi.wave_delete(wid)
+                                except KeyboardInterrupt:
+                                        pi.wave_tx_stop()
+                                        pi.wave_delete(wid)
+                                        print 'keyboard stopped'
+                                        mybreak(GPIO)
+                                        pi.stop
+
+                return pulseLen
+
+
+
+
+
+
+
+
---
+++ RaspberryPi/rcCar.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+from pigpio import turn
+from pigpio import throttle
+import myPigpio
+import time
+from types import *
+import os
+import socket
+
+'''
+This file is for running the RC car. It will create a wifi socket and listen on
+that socket for data coming from the app.
+'''
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                            INITILIZATION
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+data.txt will be used to store previous state of the motor signals sent,
+which we will initialize to 0 for both throttle and steering
+'''
+f = open("data.txt","w")
+f.write("0,0")
+f.close()
+
+#create an instance of the pigpio class, pi1 will be used to control steering
+pi1 = pigpio.pi()
+
+#steering is done on pin 23
+pi1.set_mode(23,pigpio.OUTPUT)
+
+#stop signals on pin 23 in case there is some when file started. Keeps from
+#raspberry pi crashing
+pi1.wave_tx_stop()
+base_st = []
+
+#turns wheels to "null" position at 0. 
+base_st.append(pigpio.pulse(1<<23,0,1.5*1000))
+base_st.append(pigpio.pulse(0,1<<23, 8.5*1000))
+pi1.wave_add_generic(base_st)
+st_wid = pi1.wave_create()
+if st_wid >= 0:
+        #repeate this signal until told something else
+        pi1.wave_send_repeat(st_wid)
+
+#create in instance of pigpio class, pi2 will be used to control throttle
+#everything else same as for the steering. 
+pi2 = pigpio.pi()
+pi2.set_mode(14,pigpio.OUTPUT)
+pi2.wave_tx_stop()
+base_thr = []
+base_thr.append(pigpio.pulse(1<<14,0,1.5*1000))
+base_thr.append(pigpio.pulse(0,1<<14, 8.5*1000))
+pi2.wave_add_generic(base_thr)
+th_wid = pi2.wave_create()
+if th_wid >= 0:
+        pi2.wave_send_repeat(th_wid)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#ip address of the raspberry pi, and port chosen
+HOST = "192.168.42.1"
+PORT = 5005
+
+#small buffer size for speed
+buffer_size=15
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+s.bind((HOST,PORT))
+s.listen(1)
+conn,addr = s.accept()
+print "connected by", addr
+
+while 1:
+        #recieves the data from the connection
+        data = conn.recv(buffer_size)
+        
+        if not data: break
+
+        #print "recieved data:", data, len(data)
+        #data incoming is in the form of a CSV string
+        dataSplit = data.split(",")
+
+        #data should be of these lengths depending on the values sent from app
+        if len(data) > 8 and len(data) < 15:
+                #print dataSplit[1],dataSplit[2]
+                move = int(dataSplit[1])
+                steer = int(dataSplit[2])
+                if (move >= -100 and move <=100 and steer <=100 and steer >= -100):
+                        #read the previous values of move and steer from .txt
+                        f = open("data.txt", "r")
+                        sig = -1
+                        oldData = f.readline()
+                        f.close()
+                        
+                        oldDataSplit = oldData.split(",")
+                        oldMove = int(oldDataSplit[0])
+                        oldSteer = int(oldDataSplit[1])
+                        '''
+                        #if the values of the old and new move or steer are
+                        #different than functions the respective funtions must
+                        #be called and values of old steer and new steer to be
+                        #updated
+                        '''
+
+                        ##!! TO DO move is set to half its max values, for
+                        #testing purposes, it eventually its full value
+                        if (oldSteer != steer and oldMove != move):
+                                os.remove("data.txt")
+                                fnew = open("data.txt", "w")
+                                fnew.write(str(move) + "," + str(steer))
+
+                                #sig = 1 means both steering and moved have
+                                #already been dealt with, dont need below calls
+                                sig = 1
+                                
+                                fnew.close()
+                                turn(steer,pi1)
+                                throttle(move/2,pi2)
+                                
+                        if (oldSteer != steer and sig != 1):
+                                os.remove("data.txt")
+                                fnew = open("data.txt", "w")
+                                fnew.write(str(move) + "," + str(steer))
+                                fnew.close()
+                                turn(steer,pi1)
+                                
+                        if (oldMove != move and sig != 1):
+                                os.remove("data.txt")
+                                fnew = open("data.txt", "w")
+                                fnew.write(str(move) + "," + str(steer))
+                                fnew.close()
+                                throttle(move/2,pi2)
+
+
+f.close()
+conn.close()
