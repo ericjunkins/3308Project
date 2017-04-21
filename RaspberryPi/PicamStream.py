798c46f182377eaa54ed3523884f252d810a34d4
jeow9028 <jeow9028@colorado.edu>
2017-04-21 13:33:01 -0600

    Android link to the stream

--- RaspberryPi/PicamStream.py
+++ RaspberryPi/PicamStream.py
@@ -3,12 +3,16 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.start_preview()
+camera.start_preview(
+raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8160}' :demux=h264
 
 for i in camera:
-	 
-	 v4l2-ctl --overlay=1 # enable viewfinder
-     v4l2-ctl --overlay=0 # disable viewfinder
+     
+    v4l2-ctl --overlay=1 # enable viewfinder
+    v4l2-ctl --overlay=0 # disable viewfinder
+     # Record a video
+    v4l2-ctl --set-fmt-video=width=1920,height=1088,pixelformat=4
+    v4l2-ctl --stream-mmap=3 --stream-count=100 --stream-to=somefile.264
 
 #camera.start_recording()
 sleep(10)
@@ -16,9 +20,15 @@ sleep(10)
 camera.stop_preview()
 
 
+#**********************************************
+#This needs to go to the command line
+#raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout 
+#**********************************************
+
+
 '''
-    # Load the module
-    sudo modprobe bcm2835-v4l2
+    # Load the sudo
+    module modprobe bcm2835-v4l2
 
     # Control the viewfinder
     v4l2-ctl --overlay=1 # enable viewfinder
