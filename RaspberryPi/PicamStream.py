d752de4acc2434b2d1a5c76c3659aed92673e7e3
jeow9028 <jeow9028@colorado.edu>
2017-04-21 12:47:38 -0600

    PiStreaming 1st commit

---
+++ RaspberryPi/PicamStream.py
@@ -0,0 +1,40 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+
+for i in camera:
+	 
+	 v4l2-ctl --overlay=1 # enable viewfinder
+     v4l2-ctl --overlay=0 # disable viewfinder
+
+#camera.start_recording()
+sleep(10)
+#camera.stop_recording()
+camera.stop_preview()
+
+
+'''
+    # Load the module
+    sudo modprobe bcm2835-v4l2
+
+    # Control the viewfinder
+    v4l2-ctl --overlay=1 # enable viewfinder
+    v4l2-ctl --overlay=0 # disable viewfinder
+
+    # Record a video
+    v4l2-ctl --set-fmt-video=width=1920,height=1088,pixelformat=4
+    v4l2-ctl --stream-mmap=3 --stream-count=100 --stream-to=somefile.264
+
+    # Capture a JPEG image
+    v4l2-ctl --set-fmt-video=width=2592,height=1944,pixelformat=3
+    v4l2-ctl --stream-mmap=3 --stream-count=1 --stream-to=somefile.jpg
+
+    # Set the video bitrate
+    v4l2-ctl --set-ctrl video_bitrate=10000000
+
+    # List the supported formats
+    v4l2-ctl --list-formats
+'''
